aa4f9bdd975f398b5996599afde99f2d4cd15b4a
efettf <efettf@proton.me>
2024-11-10 18:42:59 +0100

    :wrench: Add custom config file

--- config.def.h
+++ config.def.h
@@ -1,20 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                        /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy  = 1;                        /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int centered = 1;                      /* -c option; centers dmenu on screen */
+static int min_width = 300;                   /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBraisMono Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#89dceb" },
+	[SchemeOut] = { "#000000", "#89dceb" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 10;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
